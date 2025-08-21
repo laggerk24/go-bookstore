71250ddf2ce64b7f41829493daa93df61868df00
laggerk24 <laggerk24@gmail.com>
2025-08-21 23:38:03 +0530

    db connection fix

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:yourpassword@tcp(127.0.0.1:3306)/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:Crossworld@123@tcp(127.0.0.1:3306)/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
